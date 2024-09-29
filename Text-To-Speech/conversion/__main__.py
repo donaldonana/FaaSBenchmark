da21a4f8ec8df5e00e3fd84692e2dc83dc7d56fa
donaldonana <onanadonald@gmail.com>
2024-09-30 01:56:32 +0200

    text 2 speech

--- Text-To-Speech/conversion/__main__.py
+++ Text-To-Speech/conversion/__main__.py
@@ -1,6 +1,6 @@
-from gtts import gTTS
+from pydub import AudioSegment
 from io import BytesIO
-import datetime
+import os
 import swiftclient
 
 
@@ -54,28 +54,24 @@ def pull(obj, ipv4):
 
 
 def main(args):
-    
-    
-    message = 'You’re doing a fantastic job! Keep up the great work.'
-    message = 'You are such an idiot! This is a fucking mess, and you screwed everything up!'
-    message = "I can't believe this shit happened again! You're a complete asshole!"
-    message = "What a load of bullshit! You can't even get the simplest thing right."
 
     ipv4 = args.get("ipv4", "192.168.1.120")
-     
-    tts = gTTS(text=message, lang='en')
-    mp3_fp = BytesIO()
-    tts.write_to_fp(mp3_fp)
-    result = mp3_fp.getvalue()
-    
-    with open("speeech.mp3", "wb") as f:
-            f.write(result)
 
-    push("speeech.mp3", ipv4)
+    # pull("speeech.mp3", ipv4)
+    
+    # mp3file =  open("speeech.mp3", "rb").read()
+    # input = BytesIO(mp3file)
+    # speech = AudioSegment.from_mp3(input)
+    # inputSize = len(input.getvalue())
+    # output = BytesIO()
+    # speech.export(output, format="wav")
+    # result = output.getvalue()
+    
+    # with open("speeech.wav", "wb") as f:
+    #         f.write(result)
     
     return {
-         "MessageSize" : str(len(message)),
-         "FileSize" : str(len(message))
+         "Outputfilesize" : "str(len(output.getvalue()))"
          }
     
 
