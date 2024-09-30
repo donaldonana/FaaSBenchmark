b05b9741c7947ec36b2ddef6774a2bf95c110221
donaldonana <onanadonald@gmail.com>
2024-09-30 04:24:17 +0200

    try to resolve pydub problem

--- Text-To-Speech/conversion/conversion.sh
+++ Text-To-Speech/conversion/conversion.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-docker build -t action-python-v3.10:conversion .
+docker build --no-cache -t action-python-v3.10:conversion .
 
 docker tag action-python-v3.10:conversion onanad/action-python-v3.10:conversion
 
 docker push onanad/action-python-v3.10:conversion
 
-wsk action update conversion --memory 128 --docker onanad/action-python-v3.10:conversion __main__.py --web true 
+wsk action update conversion --memory 512 --docker onanad/action-python-v3.10:conversion __main__.py --web true 
 
 wsk action invoke conversion --result  --param ipv4 "192.168.1.120"
--- Text-To-Speech/tex2speech/text2speech.sh
+++ Text-To-Speech/tex2speech/text2speech.sh
@@ -5,7 +5,7 @@ docker tag action-python-v3.9:text2speech onanad/action-python-v3.9:text2speech
 
 docker push onanad/action-python-v3.9:text2speech
 
-wsk action update text2speech --memory 128 --docker onanad/action-python-v3.9:text2speech __main__.py --web true 
+wsk action update text2speech --memory 250 --docker onanad/action-python-v3.9:text2speech __main__.py --web true 
 
 wsk action invoke text2speech --result  --param ipv4 "192.168.1.120" 
 
