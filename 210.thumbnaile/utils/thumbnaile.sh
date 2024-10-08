9589b08af860cc1007b89d5e836ae26607fd8dec
donaldonana <onanadonald@gmail.com>
2024-10-08 11:53:30 +0200

    thumbnaille bench execution

--- 210.thumbnaile/utils/thumbnaile.sh
+++ 210.thumbnaile/utils/thumbnaile.sh
@@ -6,6 +6,6 @@ docker tag action-python-v3.9:thumb onanad/action-python-v3.9:thumb
 
 docker push onanad/action-python-v3.9:thumb
 
-wsk action update thumb --docker onanad/action-python-v3.9:thumb __main__.py --web true
+wsk action update thumb --docker onanad/action-python-v3.9:thumb ../__main__.py --web true
 
 wsk action invoke thumb  --result  --param bib pillow  --param file 500b.JPEG --param ipv4 "130.190.118.116" 
